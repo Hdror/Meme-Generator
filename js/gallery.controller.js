fc4044204b6abaee699135fa6f8bfdd697f071e4
Dror Handler <hdror507@gmail.com>
2021-12-20 00:54:00 +0200

    final

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -1,16 +1,21 @@
 'use strict'
 
+var gIsGalleryShown = true
+
 function onImgSelect(imgId) {
     setImg(imgId)
     renderMeme()
     document.querySelector('.gallery-container').classList.toggle("active")
     document.querySelector('.editor-container').classList.toggle("active")
     document.querySelector('.about').classList.toggle("active")
+    gIsGalleryShown = false
     // drawImgFromlocal(img, 0, 0, gCanvas.width, gCanvas.height)
 }
 
-function onGallery(){
-     document.querySelector('.gallery-container').classList.toggle("active")
-     document.querySelector('.editor-container').classList.toggle("active")
-     document.querySelector('.about').classList.toggle("active")
+function onGallery() {
+    if (gIsGalleryShown) return
+    document.querySelector('.gallery-container').classList.toggle("active")
+    document.querySelector('.editor-container').classList.toggle("active")
+    document.querySelector('.about').classList.toggle("active")
+    gIsGalleryShown = true
 }
